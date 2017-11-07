20224889137147789a460173cf53d8028904a627
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-07 22:04:35 +0000

    Indecision State: Part I - passed down method to child component. Challenge issued.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,37 @@
 class IndecisionApp extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+        this.state = {
+            options: ['Thing One', 'Thing Two', 'Thing Three']
+        }
+    }
+
+    handleDeleteOptions() {
+        this.setState(() => {
+            return {
+                options: []
+            }
+        });
+    }
+
+    // handlePick - pass down to Action and seyup onClick- bind here
+    // randomly pick an option and alert it.
+    // remove old handlePick method from component.
+
     render() {
         const title = "Indecision App";
         const subTitle = "Put Your life in the hands of a computer.";
-        const options = ["Thing One", "Thing Two", "Thing Four"];
 
         return (
             <div>
                 <Header title={title} subTitle={subTitle} />
-                <Action />
-                <Options options={options} />
+                <Action hasOptions={this.state.options.length > 0}/>
+                <Options 
+                    options={this.state.options}
+                    handleDeleteOptions={this.handleDeleteOptions} 
+                />
                 <AddOption />
             </div>
         );
@@ -35,26 +58,23 @@ class Action extends React.Component {
     render() {
         return (
             <div>
-                <button onClick={this.handlePick}>What should I do?</button>
+                <button 
+                    disabled={!this.props.hasOptions} 
+                    onClick={this.handlePick}
+                >
+                    What should I do?
+                </button>
             </div>
         );
     }
 }
 
 class Options extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleRemoveAll = this.handleRemoveAll.bind(this);
-    }
-
-    handleRemoveAll() {
-        console.log(this.props.options);
-    }
 
     render() {
         return (
             <div>
-                <button onClick={this.handleRemoveAll.bind(this)}>Remove All</button> 
+                <button onClick={this.props.handleDeleteOptions}>Remove All</button> 
                 {
                     this.props.options.map((option) => <Option key={option} optionText={option}/>)
                 }
