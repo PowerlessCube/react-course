e73a41a2487b03d516df7d1d0f56667c69f71c7b
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-07 00:30:48 +0000

    Adding State to Counter App - Challenge: added handleMinusOne functionality using setstate

--- src/playground/state-count.js
+++ src/playground/state-count.js
@@ -9,23 +9,26 @@ class Counter extends React.Component {
         this.handleReset = this.handleReset.bind(this);
         this.state = {
             count: 0,
-            name: "Some Name"
         };
     }
     
     // Methods
     handleAddOne() {
-        console.log('handleAddOne');
         this.setState((prevState) => { 
             return {
-                // we're just changing the count value, not name.
                 count: prevState.count + 1
             };
         });
     }
 
     handleMinusOne() {
-        console.log('handleMinusOne')
+        // Challenge
+        // Call this.setState decrement the count by 1.
+        this.setState((prevState) => {
+            return {
+                count: prevState.count - 1
+            }
+        });
     }
 
     handleReset() {
@@ -35,7 +38,6 @@ class Counter extends React.Component {
     render() {
         return (
             <div>
-                {this.state.name}
                 <h1>Count: {this.state.count}</h1>
                 <button onClick={this.handleAddOne}>+</button>
                 <button onClick={this.handleMinusOne}>-</button>
