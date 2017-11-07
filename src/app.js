0e7e58ae6da3fadf512cb2f041e3d8a7f51dfbfd
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-07 23:22:24 +0000

    The Stateless Functional Component - updated the Action class to a Stateless functional component

--- src/app.js
+++ src/app.js
@@ -80,19 +80,17 @@ class Header extends React.Component {
     };
 }
 
-class Action extends React.Component {
-    render() {
-        return (
-            <div>
-                <button 
-                    disabled={!this.props.hasOptions} 
-                    onClick={this.props.handlePick}
-                >
-                    What should I do?
-                </button>
-            </div>
-        );
-    }
+const Action = (props) => {
+    return (
+        <div>
+            <button 
+                disabled={!props.hasOptions} 
+                onClick={props.handlePick}
+            >
+                What should I do?
+            </button>
+        </div>
+    );
 }
 
 class Options extends React.Component {
@@ -152,20 +150,4 @@ class AddOption extends React.Component {
     }
 }
 
-/* Advantages of Stateless functional component.
-    1. they don't manage state, but they do manage props.
-    2. They do not have access to this - props are passed in as first argument.
-    3. They are faster than out class state components (use them where you can.)
-    4. Easier to read and write
-    5. Easier to test (see more later on).
-*/ 
-const User = (props) => {
-    return (
-        <div>
-            <p>Name: {props.name} </p>
-            <p>Age: {props.age}</p>
-        </div>
-    );
-};
-
-ReactDOM.render(<User name='Alistair' age={29} />, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
