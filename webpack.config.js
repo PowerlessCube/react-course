600998f4f8ca21affdec58dfeb6aa78f862a71e7
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-11 02:36:32 +0000

    Setting up Babel with Webpack - learned how to instruct webpack to run babel using the babel loader.

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,16 @@ module.exports = {
     output: { 
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
+    },
+    //loader -> transform it to babel.
+    module: {
+        // 3 properties to setup
+        rules: [{
+            loader: 'babel-loader', // loader of choice
+            test:  /\.js$/, // what file do we want to run it on
+            exclude: /node_modules/
+        }]
     }
+    // set up presets with the .babelrc
 };
+
