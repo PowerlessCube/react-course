3237ccd09911abec83c563141b94591e0d06ee89
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-11 01:27:09 +0000

    Installing & Confuguring Webpack - Tweaks to the setup to allow webpack to watch for changes.

--- webpack.config.js
+++ webpack.config.js
@@ -12,3 +12,17 @@ module.exports = {
     }
 }
 // output - where do we put the bundle.
+
+/* 
+Terminal output definitions:
+
+Hash: 16bae5e627a63ed7f971 -> integrity of the output
+Version: webpack 3.1.0
+Time: 280ms
+    Asset     Size  Chunks             Chunk Names
+bundle.js  2.51 kB       0  [emitted]  main --> output
+   [0] ./src/app.js 33 bytes {0} [built] --> input
+✨  Done in 3.16s.
+*/
+
+// with webpack setup in this way we no longer need the scripts app.js file. and chaning index.html script to take our bundle.
