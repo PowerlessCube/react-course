0dac7c41c1e2315e6dc1f05064020b8d43c0dabb
Alistair Mackay <alistair.mackay.work@gmail.com>
2017-11-12 23:44:38 +0000

    Tweaks - and onRequestClose

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -2,9 +2,11 @@
 import React from 'react';
 import Modal from 'react-modal';
 
+// onRequestClose - closes modal when esc or click out side modal happens.
 const OptionModal = (props) => (
     <Modal
         isOpen={!!props.selectedOption}
+        onRequestClose={props.handleClearSelectedOption}
         contentLabel="Selected Option"
     >
         <h3>Selected Option</h3>
